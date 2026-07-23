4c2f266fdda17777e9e9a3e4f736afcd10579e65
andres acosta <andresfelipeacostagarcia34@gmail.com>
2026-07-23 09:36:24 -0500

    success

--- infrastructure/versions.tf
+++ infrastructure/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.0"
+      version = "~> 6.28"
     }
     awscc = {
       source  = "hashicorp/awscc"
@@ -17,6 +17,10 @@ terraform {
 
 provider "aws" {
   region = var.aws_region
+  skip_credentials_validation = true
+  skip_requesting_account_id  = false
+  skip_metadata_api_check     = true
+  allowed_account_ids     = ["688567305851"]
 
   default_tags {
     tags = {
