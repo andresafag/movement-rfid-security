99032dbe9f693d11be70e9f5b0216d62d3e7e814
andres acosta <andresfelipeacostagarcia34@gmail.com>
2026-07-23 09:39:28 -0500

    success2

--- infrastructure/versions.tf
+++ infrastructure/versions.tf
@@ -16,11 +16,11 @@ terraform {
 
 
 provider "aws" {
-  region = var.aws_region
+  region                      = var.aws_region
   skip_credentials_validation = true
   skip_requesting_account_id  = false
   skip_metadata_api_check     = true
-  allowed_account_ids     = ["688567305851"]
+  allowed_account_ids         = ["688567305851"]
 
   default_tags {
     tags = {
